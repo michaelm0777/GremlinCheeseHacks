ba242affaeca9edd242a544847c0b7968b95eea7
Michael Meng <michaelmeng2007@gmail.com>
2026-02-28 15:53:18 -0600

    Initial Commit

---
+++ TestingingTests/TestingingTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestingingTests.swift
+//  TestingingTests
+//
+//  Created by Michael Meng on 2/28/26.
+//
+
+import Testing
+@testable import Testinging
+
+struct TestingingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
